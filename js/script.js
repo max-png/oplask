113bd6bf03bad7baa7faa5be294907d8badbf3d1
RobertPS <robert.p.svensson@gmail.com>
2020-04-30 01:39:22 +0200

    script: sök på input, pres: sökresultat övriga korr

--- js/script.js
+++ js/script.js
@@ -1,14 +1,36 @@
 const urlString = "https://api.unsplash.com/search/photos?query=searchTerm&page=pageNumber&per_page=itemsPerPage&client_id=Fsoh1OYOMgH1WKiD6oO2XOh87SzDSKlwKfXTEkC_kLM"
 
-async function fetchSearchResult(search, container, page, pageSize){
+const serachContainer = document.querySelector(".search-container");
+const serachInput = serachContainer.querySelector("input.searchInput");
+const searchButtons = serachContainer.querySelectorAll("button.search");
+
+const resultContainer = document.querySelector("section.imgContainer");
+
+// resultContainer.innerHTML = `<p>Hej Hej!</p>`
+
+searchButtons.forEach(button => {
+    button.addEventListener("click", (event) => {
+        let serachText = serachContainer.querySelector("input.searchInput").value;
+        fetchSearchResult(serachText, resultContainer, 1, 10)
+    });
+});
+
+serachInput.addEventListener("keyup", (event) => {
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        searchButtons[0].click();
+    }
+});
+
+async function fetchSearchResult(search, container, page, pageSize) {
     let searchUrlString = urlString.replace("searchTerm", search)
     searchUrlString = searchUrlString.replace("pageNumber", page)
     searchUrlString = searchUrlString.replace("itemsPerPage", pageSize)
 
-    let response = await fetch(searchUrlString)
-    data = await response.json()
-    console.log(data.results)
-    
+    const response = await fetch(searchUrlString) //prova const?
+    const data = await response.json()
+    // console.log(data.results)
+
     renderSearchResult(data.results, container)
 }
 
@@ -22,8 +44,6 @@ function renderSearchResult(results, container) {
 function createAndAddImage(url, container) {
     let newImage = document.createElement("img")
     newImage.src = url
-    document.querySelector(container).appendChild(newImage);
+    // document.querySelector(container).appendChild(newImage);
+    container.appendChild(newImage);
 }
-
-
-fetchSearchResult("city", "imgContainer", 1, 4)
